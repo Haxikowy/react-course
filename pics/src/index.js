3536a8d1b9edc71d66ad761f3b1d7da174d4e291
Szymon Dolnik <szymon2407@hotmail.com>
2021-08-30 18:11:09 +0200

    Initial day4 commit

---
+++ pics/src/index.js
@@ -0,0 +1,15 @@
+import './css/index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div className={'container'}>
+            <h1>Hello world!</h1>
+         </div>
+      );
+   }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
