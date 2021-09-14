5386921bef32681d8b5a2258275e760c2f871739
Szymon Dolnik <szymon2407@hotmail.com>
2021-09-15 00:24:19 +0200

    Ended the app -> using redux thunk to make api call

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,15 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async dispatch => {
   const { data } = await jsonPlaceholder.get('/posts');
@@ -8,5 +19,6 @@ export const fetchPosts = () => async dispatch => {
 
 export const fetchUser = id => async dispatch => {
   const { data } = await jsonPlaceholder.get(`/users/${id}`);
+
   dispatch({ type: 'FETCH_USER', payload: data });
 };
